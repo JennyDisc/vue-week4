2f6fda45c2933b1f093e426a84963e465f11be7e
JennyDisc <touki741@hotmail.com>
2023-03-03 23:37:05 +0800

    git init

---
+++ login.js
@@ -0,0 +1,44 @@
+// 載入vue模組(如createApp)
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.min.js';
+
+// 路由(所有API網址的開頭)
+const site = 'https://vue3-course-api.hexschool.io/v2/';
+
+// 生成在app這個位置
+const app = {
+    data() {
+        return {
+            user: {
+                username: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        login() {
+            const loginUrl = `${site}admin/signin`;
+            // axios.post(,)發送請求。第二個參數紀錄要傳送給對方伺服器的資料內容
+            axios.post(loginUrl, this.user)
+                .then(response => {
+                    console.log(response);
+
+                    // 儲存 Token
+                    const { expired, token } = response.data; // 解構寫法。表示expired與token來自response.data
+                    // hexToken自定義名稱，把這個cookies儲存起來時要用什麼名稱
+                    // expires是一般時間格式。可以用new Date() 轉型成時間格式
+                    document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
+
+                    // 轉址(轉跳到指定頁面)
+                    window.location = './products.html';
+                })
+                .catch(error => {
+                    // 驗證登入失敗跳出視窗
+                    alert(error.data.message);
+                    console.dir(error);
+                });
+        },
+    },
+};
+
+createApp(app)
+    .mount('#app')  // 生成在app這個位置
---
+++ pagination.js
@@ -0,0 +1,30 @@
+export default {
+    props: ['pages','getProducts'],
+    template: `<nav aria-label="Page navigation example">
+    <ul class="pagination">
+
+      <li class="page-item" :class="{disabled: !pages.has_pre}">
+        <a class="page-link" href="#" aria-label="Previous" @click.prevent="getProducts(pages.current_page-1)">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+
+      <li class="page-item" :class="{active: page===current_page}" v-for="page in pages.total_pages" :key="page+123">
+        <a class="page-link" href="#" @click.prevent="getProducts(page)">{{page}}</a>
+      </li>
+
+      <li class="page-item" :class="{disabled: !pages.has_next}">
+        <a class="page-link" href="#" aria-label="Next" @click.prevent="getProducts(pages.current_page+1)">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>`,
+};
+
+// 註解：當前為props寫法
+// template中三個<li>代表上一頁、頁數、下一頁
+
+// 1.頁數 <li class="page-item" :class="{active: page===current_page}" v-for="page in pages.total_pages" :key="page+123">
+// v-for="page in pages.total_pages" 取得頁數有幾頁的數字，就去跑幾次迴圈
+// :class="{active: page===current_page}" 當page===current_page時class就加上active屬性
