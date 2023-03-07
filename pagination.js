6ae2d31652d2487603ffd4112e04af2e0ffff06c
JennyDisc <touki741@hotmail.com>
2023-03-07 15:49:37 +0800

    依助教建議將分頁class屬性加上atcive

--- pagination.js
+++ pagination.js
@@ -9,7 +9,7 @@ export default {
         </a>
       </li>
 
-      <li class="page-item" :class="{active: page===current_page}" v-for="page in pages.total_pages" :key="page+123">
+      <li class="page-item" :class="{active: page===pages.current_page}" v-for="page in pages.total_pages" :key="page+123">
         <a class="page-link" href="#" @click.prevent="getProducts(page)">{{page}}</a>
       </li>
 
@@ -25,6 +25,6 @@ export default {
 // 註解：當前為props寫法
 // template中三個<li>代表上一頁、頁數、下一頁
 
-// 1.頁數 <li class="page-item" :class="{active: page===current_page}" v-for="page in pages.total_pages" :key="page+123">
+// 1.頁數 <li class="page-item" :class="{active: page===pages.current_page}" v-for="page in pages.total_pages" :key="page+123">
 // v-for="page in pages.total_pages" 取得頁數有幾頁的數字，就去跑幾次迴圈
-// :class="{active: page===current_page}" 當page===current_page時class就加上active屬性
+// :class="{active: page===pages.current_page}" 當page===pages.current_page時class就加上active屬性，當前頁面顯示藍色底
